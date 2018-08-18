b14250bbbc2aa989c44d70b24f01bb93b9b2083a
griffinhall2 <griffin.hall@yahoo.com>
2018-08-18 09:58:07 -0400

    database created

---
+++ bamazonDB.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+    item_id INT NOT NULL,
+    product_name VARCHAR(100),
+    departmen_name VARCHAR(100),
+    price DECIMAL(10,2),
+    stock_quantity INT(10) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("1", "Xbox", "Electronics", 350.00, 150);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("2", "Gone WIth the Wind", "Books", 15.50, 120);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("3", "Tide", "Household", 12.99, 80);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("4", "UHD Television", "Electronics", 1299.89, 35);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("5", "Paper Towels", "Household", 8.99, 300);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("6", "Jacket", "Clothing", 49.90, 80);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("7", "Vitamins", "Health", 19.99, 75;
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("8", "Shoes", "Clothing", 59.50, 120);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("9", "Toothbrush Pack", "Health", 8.99, 200);
+
+INSERT INTO products (item_id, product_name, departmen_name, price, stock_quantity)
+VALUES ("10", "Computer Paper", "Office", 14.95, 220);
