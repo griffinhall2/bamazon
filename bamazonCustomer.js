b9ebb681dddce2721c931a4614dc2494b01fcc19
griffinhall2 <griffin.hall@yahoo.com>
2018-08-20 20:30:55 -0400

    additions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("cli-table") 
 
 var connection = mysql.createConnection({
   host: "127.0.0.1",
@@ -11,10 +12,11 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log('connected as id' + connection.threadId + '\n\n');
+    console.log("connected as id" + connection.threadId);
         start();
 });
 
-
+var start = function() {
+    connection.query("SELECT * FROM Products", function(err, res) {
 
 
